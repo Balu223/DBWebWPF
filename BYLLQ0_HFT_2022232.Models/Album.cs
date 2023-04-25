2ea50949e8c18795e61a8275cb48f81704da9607
Balázs Szalai <szalai.balazs2002@gmail.com>
2023-04-25 23:21:00 +0200

    Album model added

---
+++ BYLLQ0_HFT_2022232.Models/Album.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace BYLLQ0_HFT_2022232.Models
+{
+    public class Album
+    {
+        public Album()
+        {
+            Songs = new HashSet<Song>();
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AlbumId { get; set; }
+        public string AlbumName { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public int? ArtistId { get; set; }
+
+        public virtual Artist Artist { get; set; }
+        public virtual ICollection<Song> Songs { get; set; }
+        public Album(string data)
+        {
+            string[] d = data.Split('#');
+            AlbumId = int.Parse(d[0]);
+            AlbumName = d[1];
+            ReleaseDate = DateTime.Parse(d[2].Replace('-','.'));
+            ArtistId = int.Parse(d[3]);
+            Songs = new HashSet<Song>();
+        }
+    }
+}
