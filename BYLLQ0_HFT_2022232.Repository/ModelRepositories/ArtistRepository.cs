a099e8c92c73133d03d5af1e2e9a09b6ba76530b
Balázs Szalai <szalai.balazs2002@gmail.com>
2023-05-02 21:23:16 +0200

    Created Repository to Artist Table

---
+++ BYLLQ0_HFT_2022232.Repository/ModelRepositories/ArtistRepository.cs
@@ -0,0 +1,31 @@
+﻿using BYLLQ0_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BYLLQ0_HFT_2022232.Repository
+{
+    public class ArtistRepository : Repository<Artist>, IRepository<Artist>
+    {
+        public ArtistRepository(MusicDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Artist Read(int id)
+        {
+            return ctx.Artists.FirstOrDefault(t => t.ArtistId == id);
+        }
+
+        public override void Update(Artist item)
+        {
+            var old = Read(item.ArtistId);
+            foreach (var property in old.GetType().GetProperties())
+            {
+                property.SetValue(old, property.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
