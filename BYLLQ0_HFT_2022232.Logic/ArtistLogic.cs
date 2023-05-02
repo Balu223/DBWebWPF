bc83ff75ac955e455f14b160234f33cd5ef82231
Balázs Szalai <szalai.balazs2002@gmail.com>
2023-05-02 21:21:58 +0200

    Created Logic to ArtistTable

---
+++ BYLLQ0_HFT_2022232.Logic/ArtistLogic.cs
@@ -0,0 +1,92 @@
+﻿using BYLLQ0_HFT_2022232.Models;
+using BYLLQ0_HFT_2022232.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BYLLQ0_HFT_2022232.Logic
+{
+    public class ArtistLogic
+    {
+        IRepository<Artist> repo;
+
+        public ArtistLogic(IRepository<Artist> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Artist item)
+        {
+            if (item.RealName.Length < 3)
+            {
+                throw new ArgumentException("Name too short.");
+            }
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Artist Read(int id)
+        {
+            var artist = this.repo.Read(id);
+            if (artist == null)
+            {
+                throw new ArgumentException("Artist doesnt exist.");
+            }
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Artist> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Artist item)
+        {
+            this.repo.Update(item);
+        }
+
+        // 5 non crud method
+        //Albumonként össz-zene
+
+        //azok közül akik az interscopenál vannak kinek van több száma
+        //labelenként összzene
+        //össz zeneszám artistonként
+
+
+
+        //Songs by Label
+        public IEnumerable<Song> GetSongsByLabel(int labelId)
+        {
+            var songs = this.repo.ReadAll()
+                    .Where(a => a.Label.LabelId == labelId)
+                    .SelectMany(a => a.Albums)
+                    .SelectMany(al => al.Songs)
+                    .ToList();
+
+                return songs;
+            
+        }
+
+        public List<Artist> GetArtistsByGenre(string genre)
+        {
+
+                var artistsByGenre = this.repo.ReadAll()
+                    .Where(a => a.Songs.Any(al => al.Genre == genre))
+                    .ToList();
+
+                return artistsByGenre; 
+        }
+
+
+
+    }
+}
