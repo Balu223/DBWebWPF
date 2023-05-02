bc146d217a99a58fbf5c7209fa2605a6ea9cf708
Balázs Szalai <szalai.balazs2002@gmail.com>
2023-05-02 21:23:30 +0200

    Created Repository to Label Table

---
+++ BYLLQ0_HFT_2022232.Repository/ModelRepositories/LabelRepository.cs
@@ -0,0 +1,31 @@
+﻿using BYLLQ0_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BYLLQ0_HFT_2022232.Repository
+{
+    public class LabelRepository : Repository<Label>, IRepository<Label>
+    {
+        public LabelRepository(MusicDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Label Read(int id)
+        {
+            return ctx.Labels.FirstOrDefault(t => t.LabelId == id);
+        }
+
+        public override void Update(Label item)
+        {
+            var old = Read(item.LabelId);
+            foreach (var property in old.GetType().GetProperties())
+            {
+                property.SetValue(old, property.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
