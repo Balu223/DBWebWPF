3d67eceb90f976f76d39efd4febbc594153551c3
Balázs Szalai <szalai.balazs2002@gmail.com>
2023-05-02 21:23:49 +0200

    Created Repository to Song Table

---
+++ BYLLQ0_HFT_2022232.Repository/ModelRepositories/SongRepository.cs
@@ -0,0 +1,31 @@
+﻿using BYLLQ0_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BYLLQ0_HFT_2022232.Repository
+{
+    public class SongRepository : Repository<Song>, IRepository<Song>
+    {
+        public SongRepository(MusicDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Song Read(int id)
+        {
+            return ctx.Songs.FirstOrDefault(t => t.SongId == id);
+        }
+
+        public override void Update(Song item)
+        {
+            var old = Read(item.SongId);
+            foreach (var property in old.GetType().GetProperties())
+            {
+                property.SetValue(old, property.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
