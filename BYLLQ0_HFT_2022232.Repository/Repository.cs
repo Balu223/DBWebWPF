3edc12ab1e353dd2552834b056a0d70392b6e84b
Balázs Szalai <szalai.balazs2002@gmail.com>
2023-05-02 21:26:24 +0200

    Created general foundation to Repository

---
+++ BYLLQ0_HFT_2022232.Repository/Repository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace BYLLQ0_HFT_2022232.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected MusicDbContext ctx;
+
+        public Repository(MusicDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public abstract T Read(int id);
+        public abstract void Update(T item);
+    }
+}
