43ed73a7b438e1799c7e0e815811798d15cbfc87
Balázs Szalai <szalai.balazs2002@gmail.com>
2023-05-02 21:22:58 +0200

    Created Repository to Album Table

---
+++ BYLLQ0_HFT_2022232.Repository/ModelRepositories/AlbumRepository.cs
@@ -0,0 +1,31 @@
+﻿using BYLLQ0_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BYLLQ0_HFT_2022232.Repository
+{
+    public class AlbumRepository : Repository<Album>, IRepository<Album>
+    {
+        public AlbumRepository(MusicDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Album Read(int id)
+        {
+            return ctx.Albums.FirstOrDefault(t => t.AlbumId == id);
+        }
+
+        public override void Update(Album item)
+        {
+            var old = Read(item.AlbumId);
+            foreach (var property in old.GetType().GetProperties())
+            {
+                property.SetValue(old, property.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
