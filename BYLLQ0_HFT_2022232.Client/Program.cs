599cf2b457d3fdf0fb7e341d9b57e3fc323c5461
Balázs Szalai <szalai.balazs2002@gmail.com>
2023-05-04 12:56:41 +0200

    Added basic menu integration

--- BYLLQ0_HFT_2022232.Client/Program.cs
+++ BYLLQ0_HFT_2022232.Client/Program.cs
@@ -1,7 +1,11 @@
-﻿using BYLLQ0_HFT_2022232.Models;
+﻿using BYLLQ0_HFT_2022232.Logic;
+using BYLLQ0_HFT_2022232.Models;
 using BYLLQ0_HFT_2022232.Repository;
+using ConsoleTools;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace BYLLQ0_HFT_2022232.Client
 {
@@ -9,35 +13,76 @@ namespace BYLLQ0_HFT_2022232.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var ctx = new MusicDbContext();
 
-            MusicDbContext ctx = new MusicDbContext();
+            var labelrepo = new LabelRepository(ctx);
+            var artistrepo = new ArtistRepository(ctx);
+            var albumrepo = new AlbumRepository(ctx);
+            var songrepo = new SongRepository(ctx);
 
-            foreach (var album in ctx.Albums) {
-                var albumn = album;
-                foreach (var song in ctx.Songs)
-                {
-                    if (albumn.AlbumId == song.AlbumId)
-                    {
-                        Console.WriteLine(albumn.AlbumName + " " + song.SongName);
-                    }
-                }
-            }
-            //IRepository<Artist> repo = new ArtistRepository(new MusicDbContext());
-            //Artist a = new Artist()
-            //{
-            //    RealName = "Fasz",
-            //    StageName = "Kalap",
-            //    DateOfBirth = DateTime.Now,
-            //    LabelId = 1,
-            //};
-            //repo.Create(a);
-            //var another = repo.Read(2);
-            //another.RealName = "Kutya";
-            //repo.Update(another);
-            //var items = repo.ReadAll().ToArray();
-            //;
+            var labellogic = new LabelLogic(labelrepo);
+            var artistlogic = new ArtistLogic(artistrepo);
+            var albumlogic = new AlbumLogic(albumrepo);
+            var songlogic = new SongLogic(songrepo);
 
+
+            var labelSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Label"))
+                .Add("Create", () => Create("Label"))
+                .Add("Delete", () => Delete("Label"))
+                .Add("Update", () => Update("Label"))
+                .Add("Exit", ConsoleMenu.Close);
+
+            var artistSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Artist"))
+                .Add("Create", () => Create("Artist"))
+                .Add("Delete", () => Delete("Artist"))
+                .Add("Update", () => Update("Artist"))
+                .Add("Exit", ConsoleMenu.Close);
+
+            var albumSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Album"))
+                .Add("Create", () => Create("Album"))
+                .Add("Delete", () => Delete("Album"))
+                .Add("Update", () => Update("Album"))
+                .Add("Exit", ConsoleMenu.Close);
+
+            var songSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Song"))
+                .Add("Create", () => Create("Song"))
+                .Add("Delete", () => Delete("Song"))
+                .Add("Update", () => Update("Song"))
+                .Add("Exit", ConsoleMenu.Close);
+
+            var menu = new ConsoleMenu(args, level: 0)
+                .Add("Labels", () => labelSubMenu.Show())
+                .Add("Artists", () => artistSubMenu.Show())
+                .Add("Albums", () => albumSubMenu.Show())
+                .Add("Songs", () => songSubMenu.Show())
+                .Add("Exit", ConsoleMenu.Close);
+
+            menu.Show();
+        }
+
+
+        private static void Create(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Delete(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Update(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void List(string v)
+        {
+            throw new NotImplementedException();
         }
     }
 }
