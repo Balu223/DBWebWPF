8d69924f450940d5fc5a6f985e7c33bc125406dd
Balázs Szalai <szalai.balazs2002@gmail.com>
2023-11-09 19:16:42 +0100

    Created a basic ViewModel for the Main Window

---
+++ WPF_Client/MainWindowViewModel.cs
@@ -0,0 +1,58 @@
+﻿using BYLLQ0_HFT_2022232.Models;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_Client
+{
+    public class MainWindowViewModel : ObservableRecipient
+    {
+
+        public RestCollection<Artist> Artists { get; set; }
+        private Artist selectedArtist;
+
+        public Artist SelectedArtist
+        {
+            get { return selectedArtist; }
+            set
+            {
+                SetProperty(ref selectedArtist, value);
+                (DeleteArtistCommand as RelayCommand).NotifyCanExecuteChanged();
+            }
+
+
+        }
+
+
+        public ICommand CreateArtistCommand { get; set; }
+        public ICommand DeleteArtistCommand { get; set; }
+        public ICommand UpdateArtistCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Artists = new RestCollection<Artist>("http://localhost:5124/", "artist");
+            CreateArtistCommand = new RelayCommand(() =>
+            {
+                Artists.Add(new Artist()
+                {
+                    StageName = "Lil Kubik A Creeper",
+                    RealName = "Kubik Marcell"
+                });
+            });
+            DeleteArtistCommand = new RelayCommand(() =>
+            {
+                Artists.Delete(SelectedArtist.ArtistId);
+                
+            },
+            () =>
+            {
+                return SelectedArtist != null;
+            });
+        }
+    }
+}
