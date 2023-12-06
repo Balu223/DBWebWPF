6b0b0a04ea0f2259210d347652545871c9b9f6f6
Balázs Szalai <szalai.balazs2002@gmail.com>
2023-12-06 22:56:49 +0100

    Created the main page

---
+++ JSClient/wwwroot/index.js
@@ -0,0 +1,27 @@
+﻿const menu = document.getElementById("menu");
+
+
+function linker(id) {
+    switch (true) {
+        case id == 'artists':
+            window.location.href = "./Artist.html";
+            break;
+        case id == 'albums':
+            window.location.href = "./Album.html";
+            break;
+        case id == 'songs':
+            window.location.href = "./Song.html";
+            break;
+        case id == 'labels':
+            window.location.href = "./Label.html";
+            break;
+        case id == 'noncruds':
+            window.location.href = "./NonCRUD.html";
+            break;
+        case id == 'menu':
+            window.location.href = "./index.html";
+            break;
+        default:
+            break;
+    }
+}
